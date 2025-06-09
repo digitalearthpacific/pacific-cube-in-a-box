2d256c775b07cde3121b0317cf069c6137a6cbda
KammySPC <kamsinr@spc.int>
2025-06-10 11:17:07 +1200

    updated mangrove styling

--- ows/ows_config/mangroves/ows_mangroves_cfg.py
+++ ows/ows_config/mangroves/ows_mangroves_cfg.py
@@ -1,9 +1,9 @@
 from ows_config.common.ows_reslim_cfg import reslim_continental
 
-style_mangroves = {
-    "name": "mangroves",
-    "title": "Mangroves",
-    "abstract": "Mangroves",
+style_canopy = {
+    "name": "style_canopy",
+    "title": "Mangrove Canopy Cover",
+    "abstract": "Mangrove Canopy Cover",
     "needed_bands": ["mangroves"],
     "index_function": {
         "function": "datacube_ows.band_utils.single_band",
@@ -12,13 +12,32 @@ style_mangroves = {
             "band": "mangroves",
         },
     },
-    "color_ramp": [
-        {"value": 0, "color": "#BDBDBD"},
-        {"value": 1, "color": "#5ECC00"},
-        {"value": 2, "color": "#3B7F00"},
-    ],
-    "legend": {},
+    "value_map": {
+        "mangroves": [
+            {
+                "title": "No Mangrove Canopy",
+                "abstract": "",
+                "values": [0],
+                "alpha": 1.0,
+                "color": "#BDBDBD",
+            },
+            {
+                "title": "Sparse Mangrove Canopy",
+                "abstract": "",
+                "values": [1],
+                "color": "#5ECC00",
+            },
+            {
+                "title": "Dense Mangrove Canopy",
+                "abstract": "",
+                "values": [2],
+                "color": "#3B7F00",
+            }
+        ]
+    },
+    "legend": {"width": 2.5, "height": 1.0},
 }
+
 
 layer = {
     "title": "Mangroves",
@@ -46,3 +65,4 @@ Todo...
         "styles": [style_mangroves],
     },
 }
+
